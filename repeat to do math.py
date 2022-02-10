5438daf9b300830b95b31f9d91fb15907e981a7c
Mrnuke <64882625+Mrnuke000@users.noreply.github.com>
2022-02-10 14:40:56 -0800

    Repeating python script projects.

---
+++ repeat to do math.py
@@ -0,0 +1,16 @@
+start = 1
+end = 5
+
+print('some number math')
+
+for i in range(start, end + 1):
+
+  half = i / 2
+  halfNoRem = i // 2
+  square = i * i
+
+  print('i = ' + str(i))
+
+  print('half no rem = ' + str(halfNoRem))
+  print('square = ' + str(square))
+  print('half = ' + str(half))'
---
+++ repeating counter.py
@@ -0,0 +1,7 @@
+import replit
+import time
+
+for i in range(10001):
+  replit.clear() 
+  print(i)
+  time.sleep(.0001)
